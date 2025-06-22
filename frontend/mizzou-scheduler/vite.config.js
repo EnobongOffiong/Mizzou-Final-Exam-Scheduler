4c5b0080e4b1b49c3d0ffff2cdf630310f1b1b57
EnobongOffiong <edoffiong@gmail.com>
2025-06-21 19:00:15 -0500

    Updated packages and configs

--- frontend/mizzou-scheduler/vite.config.js
+++ frontend/mizzou-scheduler/vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: 'automatic' })],
   server: {
     open: true
-  }
-})
+  },
+  test: {
+    globals: true,       // Use Vitest's global APIs (e.g., `expect`, `describe`)
+    environment: 'jsdom', // Simulate a browser environment for DOM testing
+  },
+});
+  
