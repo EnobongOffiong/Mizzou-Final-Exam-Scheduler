83204ee605b3501c2deb84b38a7541f83f3b3063
EnobongOffiong <edoffiong@gmail.com>
2025-06-30 10:00:41 -0500

    added browser router to page rendering in search and home tests to match the react router dom usage in jsx

--- frontend/mizzou-scheduler/tests/Home.test.jsx
+++ frontend/mizzou-scheduler/tests/Home.test.jsx
@@ -3,17 +3,25 @@ import { render, screen } from '@testing-library/react'
 import { describe, it, expect } from 'vitest';
 import "@testing-library/jest-dom/vitest";
 import Home from '../src/components/Home';
+import { BrowserRouter } from 'react-router-dom';
 
 
 describe('Home', () => {
     it('should render the home page', () => {
-        render(<Home/>);
+        render( <BrowserRouter>
+            <Home />
+          </BrowserRouter>)
        
+
          expect(screen.getByText(/Welcome to Mizzou Exam Scheduler!/)).toBeInTheDocument();
          expect(screen.getByText(/Find your exam date by searching with the course name or class time.Then, export it to your personal calendar with ease!/))
             .toBeInTheDocument()
          const button = screen.getByRole('link', {name: /GET STARTED/i})
          expect(button).toBeInTheDocument()
+         
+
+
+        
 
     });
 });
--- frontend/mizzou-scheduler/tests/Search.test.jsx
+++ frontend/mizzou-scheduler/tests/Search.test.jsx
@@ -3,11 +3,14 @@ import { render, screen } from '@testing-library/react'
 import { describe, it, expect } from 'vitest';
 import "@testing-library/jest-dom/vitest";
 import Search from '../src/components/Search';
+import { BrowserRouter } from 'react-router-dom';
 
 
 describe('Search', () => {
     it('should render the search page', () => {
-        render(<Search/>)
+        render( <BrowserRouter>
+            <Search />
+          </BrowserRouter>)
 
         expect(screen.getByText(/SEARCH BY COURSE|SEARCH BY MEETING TIME/i)).toBeInTheDocument()
         expect(screen.getByRole('button', {name: /ADD TO CALENDAR/i})).toBeInTheDocument()
