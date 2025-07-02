632b46b8762e274eaade22c76bff71ceb60b2902
EnobongOffiong <edoffiong@gmail.com>
2025-07-01 22:31:06 -0500

    Edited analytics import

--- frontend/mizzou-scheduler/src/App.jsx
+++ frontend/mizzou-scheduler/src/App.jsx
@@ -10,7 +10,7 @@ import Home from './components/Home.jsx'
 import Search from './components/Search.jsx'
 import Calendar from './components/Calendar.jsx'
 import Footer from './components/Marquee.jsx'
-import { Analytics } from '@vercel/analytics';
+// import { Analytics } from '@vercel/analytics';
 
 function App() {
   
@@ -24,7 +24,7 @@ function App() {
         <Route path='/search' element={<Search  /> }></Route>
         <Route path='/calendar' element={<Calendar />}></Route>
       </Routes>
-      <Analytics />
+      {/* <Analytics /> */}
     </div>
   )
 }
